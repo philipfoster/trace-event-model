e5bc71c50700b4cbdb310507287ac03ec4a7317c
Philip <pfoster@redhat.com>
2020-06-18 09:30:29 -0500

    Explicitly import Process data object to prevent java.lang.Process from being used

--- src/main/java/com/redhat/eventmodel/events/ProcessTraceEvent.java
+++ src/main/java/com/redhat/eventmodel/events/ProcessTraceEvent.java
@@ -3,29 +3,30 @@ package com.redhat.eventmodel.events;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.redhat.eventmodel.model.Process;
 
 
 @JsonPropertyOrder({"ID", "TimeStamp", "EventType", "Type", "Process"})
 public class ProcessTraceEvent extends TraceEvent {
 
-	private ProcessTraceEventType type = ProcessTraceEventType.None;
-	private Process process;
+    private ProcessTraceEventType type = ProcessTraceEventType.None;
+    private Process process;
 
-	@JsonProperty("Type")
-	public ProcessTraceEventType getType() {
-		return this.type;
-	}	
+    @JsonProperty("Type")
+    public ProcessTraceEventType getType() {
+        return this.type;
+    }
 
-	public void setType(ProcessTraceEventType type) {
-		this.type = type;
-	}
+    public void setType(ProcessTraceEventType type) {
+        this.type = type;
+    }
 
-	@JsonProperty("Process")
-	public Process getProcess() {
-		return this.process;
-	}	
+    @JsonProperty("Process")
+    public Process getProcess() {
+        return this.process;
+    }
 
-	public void setProcess(Process process) {
-		this.process = process;
-	}
+    public void setProcess(Process process) {
+        this.process = process;
+    }
 }
